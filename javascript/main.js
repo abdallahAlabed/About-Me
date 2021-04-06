612205fbf9547809c03ebb95e5d861807165d160
abdallahAlabed <abdallah.s.alabd@gmail.com>
2021-04-06 17:41:12 +0300

    lab c5

--- javascript/main.js
+++ javascript/main.js
@@ -1,100 +1,219 @@
 'use strict';
 
+let q = "";
+
+let q1 = "";
+
+let q2 = "";
+
+let q3 = "";
+
+let q4 = "";
+
+let q5 = "";
+
 let mark = 0;
 
-let name = prompt("what is your name ", "abdallah");
+let score = 0;
 
-let q = prompt("do like Friends series", "yes,no");
+let number = 0;
 
-if (q.toLowerCase() == "yes" || q.toUpperCase() == "Y") {
+let randomNumber = Math.floor((Math.random() * 10) + 1);
 
-    mark++;
+let question = new Array("Rachel Green","Emily Waltham","Carol Willick");
 
-    console.log("how dose not ");
+let namee = prompt("what is your namee ", "abdallah");
 
-    alert("how dose not ");
+isRghit();
 
-    let q1 = prompt("is monica geller word i know", "yes,no");
+function isRghit() {
+    q = prompt("do like Friends series", "yes,no");
 
-    if (q1.toLowerCase() == "yes" || q1.toUpperCase() == "Y") {
-
-        mark++;
-
-        console.log("we all know that");
-
-        alert("we all know that");
-
-    } else {
-
-        console.log("you are wrong");
-
-        alert("you are wrong");
-
-    }
-
-    let q2 = prompt("is ross geller word i am fine", "yes,no");
-
-    if (q2.toLowerCase() == "yes" || q2.toUpperCase() == "Y") {
+    if (q.toLowerCase() == "yes" || q.toUpperCase() == "Y") {
 
         mark++;
 
-        console.log("and he is crying after it");
+        console.log("how dose not ");
 
-        alert("and he is crying after that");
+        alert("how dose not ");
 
+        isRghit1();
+
+
+        function isRghit1() {
+            q1 = prompt("is monica geller word i know", "yes,no");
+
+            if (q1.toLowerCase() == "yes" || q1.toUpperCase() == "Y") {
+
+                mark++;
+
+                console.log("we all know that");
+
+                alert("we all know that");
+
+                isRghit2();
+
+            } else {
+
+                console.log("you are wrong");
+
+                alert("you are wrong");
+
+                isRghit1();
+
+
+            }
+        }
+
+        function isRghit2() {
+            q2 = prompt("is ross geller word i am fine", "yes,no");
+
+            if (q2.toLowerCase() == "yes" || q2.toUpperCase() == "Y") {
+
+                mark++;
+
+                console.log("and he is crying after it");
+
+                alert("and he is crying after that");
+
+                isRghit3();
+
+            } else {
+
+                console.log("you are wrong");
+
+                alert("you are wrong");
+
+                isRghit2();
+
+            }
+        }
+
+        function isRghit3() {
+            q3 = prompt("did rachel and ross was on a break", "yes,no");
+
+            if (q3.toLowerCase() == "yes" || q3.toUpperCase() == "Y") {
+
+                mark++;
+
+                console.log("of course");
+
+                alert("of course");
+
+                isRghit4();
+
+            } else {
+
+                console.log("think again");
+
+                alert("think again");
+
+                isRghit3();
+
+            }
+        }
+
+        function isRghit4() {
+            q4 = prompt("Is how you doing sentence that joey  ues it when he want to catch a girl", "yes,no");
+
+            if (q4.toLowerCase() == "yes" || q4.toUpperCase() == "Y") {
+
+                mark++;
+
+                console.log("and its work every time");
+
+                alert("and its work every time");
+
+                isRghit5();
+
+            } else {
+
+                console.log("you dont know anything");
+
+                alert("you dont know anything");
+
+                isRghit4();
+
+
+            }
+        }
     } else {
 
-        console.log("you are wrong");
+        console.log("OMG");
 
-        alert("you are wrong");
+        alert("you are not welcome her");
 
-    }
+        isRghit();
 
-    let q3 = prompt("did rachel and ross was on a break", "yes,no");
-
-    if (q3.toLowerCase() == "yes" || q3.toUpperCase() == "Y") {
-
-        mark++;
-
-        console.log("of course");
-
-        alert("of course");
-
-    } else {
-
-        console.log("think again");
-
-        alert("think again");
 
     }
+}
 
-    let q4 = prompt("Is how you doing sentence that joey  ues it when he want to catch a girl", "yes,no");
+function isRghit5() {
+    for (let index = 0; index < 4; index++) {
 
-    if (q4.toLowerCase() == "yes" || q4.toUpperCase() == "Y") {
+        number = parseInt(prompt("try to guess the number between 0 - 10 ", 0));
 
-        mark++;
+        if (number == randomNumber) {
+            alert("excellent " + namee);
+            mark++;
+            break;
+        } else if (number < randomNumber) {
 
-        console.log("and its work every time");
+            alert("try again " + namee + " is bigger than this");
 
-        alert("and its work every time");
+            score++;
 
-    } else {
+        } else {
 
-        console.log("you dont know anything");
+            alert("try again " + namee + " is smaller than this");
 
-        alert("you dont know anything");
+            score++;
+
+        }
+
 
     }
-} else {
+}
 
-    console.log("OMG");
 
-    alert("you are not welcome her");
+let i =0;
 
+while(i<6){
+    q5 = prompt("what is the name of ross wives ? ");
+
+    for (let index = 0; index < question.length; index++) {
+        if(q5==question[index]){
+            alert("Hello " + namee + " your guess is right  ");
+            mark++;
+            i = 6;
+            break;
+        }
+        
+            
+        
+        
+    }
+    i++;
 
 }
-console.log("Hello " + name + " your right answer 5/" + mark);
 
-alert("Hello " + name + " your right answer 5/" + mark);
+console.log("Hello " + namee + " your right answer 7/" + mark);
 
-document.write("<h3>Hello " + name + "!</h1><p>Have a nice day!</p>");
+alert("Hello " + namee + " your right answer 7/" + mark);
+
+if (score != 0) {
+
+    console.log("Hello " + namee + " your wrnog guess    4/" + score);
+
+    alert("Hello " + namee + " your wrnog guess    4/" + score);
+
+} else {
+
+    console.log("Hello " + namee + " excellent job you do it from the first time");
+
+    alert("Hello " + namee + " excellent job you do it from the first time");
+}
+
+
+document.write("<h3>Hello " + namee + "!</h1><p>Have a nice day!</p>");
